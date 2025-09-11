57fd960fd683f0b72beca4aa7e53bf6a6f09732a
Emmanuel-Bawuni-Yanwube <ebawuni155@gmail.com>
2025-09-11 15:48:24 +0000

    new push

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -1,9 +1,29 @@
 using System;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "microsoft";
+        job1._startYear = 2024;
+        job1._endYear = 2025;
+
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2._company = "Apple";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Emmanuel Bawuni";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
