99f41e7ab07a472af0f65edde891a90a5fa3a8c7
Emmanuel-Bawuni-Yanwube <ebawuni155@gmail.com>
2025-10-16 20:44:36 +0000

    week 7 project

---
+++ week07/ExerciseTracking/Activity.cs
@@ -0,0 +1,28 @@
+using System;
+
+public abstract class Activity
+{
+    private DateTime _date;
+    private int _minutes;
+
+    protected Activity(DateTime date, int minutes)
+    {
+        _date = date;
+        _minutes = minutes;
+    }
+
+    public DateTime Date => _date;
+    public int Minutes => _minutes;
+
+    // Abstract methods for derived classes to implement
+    public abstract double GetDistance(); // in km
+    public abstract double GetSpeed();    // in kph
+    public abstract double GetPace();     // min per km
+
+    // Shared summary method
+    public virtual string GetSummary()
+    {
+        return $"{Date:dd MMM yyyy} {this.GetType().Name} ({Minutes} min): " +
+               $"Distance {GetDistance():0.0} km, Speed {GetSpeed():0.0} kph, Pace: {GetPace():0.00} min per km";
+    }
+}
